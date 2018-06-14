ecc7e537ef6aa0323426c18d3a5e06d063483dd0
Brett Downing <BrettRD@users.noreply.github.com>
2018-06-14 23:04:39 +1000

    fixes write past strip, adds multi refresh
    Fixes a bug where only the first frame would land on the strip.
    Refreshes the display multiple times per frame of the animation.

--- Jacket_Collar_2.ino
+++ Jacket_Collar_2.ino
@@ -9,6 +9,8 @@ uint8_t  wait = 10;
 const int nFrames = 3; //number of frames
 const int height = 4;  //height of frame
 const int width = 28;   //width of frame
+const int refreshesPerFrame = 5; //number of flicker cycles per animation frame
+
 
 void setup() {
   
@@ -56,32 +58,35 @@ uint8_t anim[nFrames][height][width][3] =
     uint8_t* pixel;  //a variable to point to a pixel in the animation
  
     for(int frame=0; frame<nFrames; frame++){ //loop through the frames
+      for(int ref=0; ref<refreshesPerFrame; ref++){
         for(int row = 0; row<height; row++){  //row by row
-            for(int col=0; col<width; col++){ //one pixel at a time
-                //select the pixel
-                pixel = anim[frame][row][col]; 
+          for(int col=0; col<width; col++){ //one pixel at a time
+            //select the pixel
+            pixel = anim[frame][row][col]; 
               
-                // split the channels
-                int red = pixel[0]; 
-                int green = pixel[1];
-                int blue = pixel[2];
+            // split the channels
+            int red = pixel[0]; 
+            int green = pixel[1];
+            int blue = pixel[2];
               
-                //red = red + random(0,20); //inject some noise on the red channel
+            //red = red + random(0,20); //inject some noise on the red channel
               
-                //figure out which led on the strip
-                int ledPos = col + (row*(width)) + (frame*(height*width));
+            //figure out which led on the strip
+            int ledPos = col + (row*(width));
                    
-                //set the colour
-                strip.setPixelColor(
-                  ledPos,
-                  red,
-                  green,
-                  blue
-                );
-            }
-        }
+            //set the colour
+            strip.setPixelColor(
+              ledPos,
+              red,
+              green,
+              blue
+            );
+            
+          }//row complete
+        }//refresh complete
         delay(wait);   //10fps maybe
         strip.show();
-    }
+      }//frame complete
+    }//animation complete
   
 }
